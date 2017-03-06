602826d41f434045e3871c6e77b897508ed263cc
lsw <dididas@naver.com>
2017-03-06 10:44:21 +0900

    added output "Tell Your World"

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
-	 system.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Tell Your World");
 	}
 
 
